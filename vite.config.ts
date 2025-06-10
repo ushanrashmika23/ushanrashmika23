7c87888f93d4178c91f94d1b333e9d6fe4b12701
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-06-10 14:09:11 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import ghPages from 'vite-plugin-gh-pages';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/ushanrashmika23/',
 })
